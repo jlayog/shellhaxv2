f92ee53d6d00a2037732b1ed889440e1a5126774
Francis Calizo <francisacalizo@gmail.com>
2019-09-21 16:42:01 -0400

    Create Signup Component

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
+import Signup from './components/auth/Signup'
+
 
 function App() {
   return (
@@ -11,6 +13,7 @@ function App() {
         <Navbar/>
         <Switch>
           <Route exact path="/" component={Home} />
+          <Route exact path="/Signup" component={Signup} />
         </Switch>
       </div>
     </Router>
--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -4,6 +4,7 @@ const Home = () => {
   return (
     <div>
       <h1 className='text-center'>Food4All</h1>
+      <p className="text-center">Come donate your fewdz</p>
     </div>
   )
 }
