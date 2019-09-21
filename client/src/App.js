a61694968e584752ceb1d734c80598d945e9d429
Francis Calizo <francisacalizo@gmail.com>
2019-09-21 16:50:10 -0400

    Implement Login Component

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,9 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
-import Signup from './components/auth/Signup'
+import Signup from './components/auth/Signup';
+import Login from './components/auth/Login';
+
 
 
 function App() {
@@ -14,6 +16,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/Signup" component={Signup} />
+          <Route exact path="/Login" component={Login} />
         </Switch>
       </div>
     </Router>
